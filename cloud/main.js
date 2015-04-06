d787032cf913fd6c3b0b352129d9d6be349663fd
Ryan D'souza <dsouzarc@gmail.com>
2015-04-06 14:34:25 -0400

    Stopped saving data on Parse

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,37 @@
+Parse.Cloud.define("addUser", function(request, response) {
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+    var UserClass = Parse.Object.extend("PromMeUser");
+    var user = new UserClass();
+
+    user.set("userName", request.params.userName);
+    user.set("facebookID", request.params.fbID);
+    user.set("userID", request.params.userID);
+
+    user.save(null, {
+        success: function(result) {
+            response.success("YES");
+        }, error: function(error) {
+            console.log(error);
+            response.error(error);
+        }
+    });
+});
+
+Parse.Cloud.define("userExists", function(request, response) {
+
+    var query = new Parse.Query("PromMeUser");
+    query.equalTo("facebookID", request.params.facebookID);
+
+    query.find({
+        success: function(results) {
+            if(results.length > 0) {
+                response.success("YES");
+            }
+            else {
+                response.success("NO");
+            }
+        }, error: function(error) {
+            response.error(error);
+        }
+    });
 });
