f36c6d1f35a9743067e43997a156785b8f9074e7
Ryan D'souza <dsouzarc@gmail.com>
2015-04-12 01:00:01 -0400

    Successfully saves new user

--- cloud/main.js
+++ cloud/main.js
@@ -1,17 +1,22 @@
 Parse.Cloud.define("addUser", function(request, response) {
 
+    Parse.Cloud.useMasterKey();
+
     var UserClass = Parse.Object.extend("PromMeUser");
     var user = new UserClass();
 
-    user.set("userName", request.params.userName);
-    user.set("facebookID", request.params.fbID);
-    user.set("userID", request.params.userID);
+    user.set("users_name", request.params.userFullName);
+    user.set("users_facebook_id", request.params.fbID);
+    user.set("users_highschool", request.params.school);
+    user.set("users_phone_number", request.params.phoneNumber);
+    user.set("users_gender", request.params.gender);
+    user.set("users_grade", request.params.grade);
+    user.set("users_hometown_geopoint", request.params.currentLocation);
 
     user.save(null, {
         success: function(result) {
             response.success("YES");
         }, error: function(error) {
-            console.log(error);
             response.error(error);
         }
     });
